a63147ab2558f7003751c43b376339dbea669f85
Nick Young <nick.young@auckland.ac.nz>
2018-04-20 15:35:42 +1200

    disable backface culling, snap to photosphere pos on click

--- 3D_campus_b4w_c.js
+++ 3D_campus_b4w_c.js
@@ -5,10 +5,20 @@ b4w.register("3D_campus_b4w_c_main", function(exports, require) {
 
 // import modules used by the app
 var m_app       = require("app");
+var m_cam       = require("camera");
 var m_cfg       = require("config");
 var m_data      = require("data");
 var m_preloader = require("preloader");
 var m_ver       = require("version");
+
+var m_anim      = require("animation");
+var m_cont      = require("container");
+var m_mouse     = require("mouse");
+var m_scenes    = require("scenes");
+var m_trans     = require("transform");
+var m_cam_anim  = require("camera_anim");
+
+var cam;
 
 // detect application mode
 var DEBUG = (m_ver.type() == "DEBUG");
@@ -77,8 +87,32 @@ function load_cb(data_id, success) {
 
     m_app.enable_camera_controls();
 
-    // place your code here
+    cam = m_scenes.get_active_camera();
+    var photosphere = m_scenes.get_object_by_name("photosphere");
+    var photospherePos = m_trans.get_translation(photosphere);
 
+    m_cam.hover_setup(cam, {pivot: photospherePos});
+
+    // place your code here
+    var canvas_elem = m_cont.get_canvas();
+    canvas_elem.addEventListener("mousedown", main_canvas_click, false);
+    canvas_elem.addEventListener("touchstart", main_canvas_click, false);
+
+}
+
+function main_canvas_click(e) {
+    if (e.preventDefault)
+        e.preventDefault();
+    var x = m_mouse.get_coords_x(e);
+    var y = m_mouse.get_coords_y(e);
+
+    var obj = m_scenes.pick_object(x, y);
+    var pos = m_trans.get_translation(obj);
+
+    if (obj) {
+        console.log("click", obj);
+        m_cam.eye_setup(cam, {pos: pos});
+    }
 }
 
 
